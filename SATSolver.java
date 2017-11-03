6c2ae28b96c62b3655a1a70d886698fbaa245e74
Skool-handle <32790402+Skool-handle@users.noreply.github.com>
2017-11-03 13:24:19 +0800

    Update SATSolver.java

--- SATSolver.java
+++ SATSolver.java
@@ -17,20 +17,20 @@ public class SATSolver {
      * unit propagation. The returned environment binds literals of class
      * bool.Variable rather than the special literals used in clausification of
      * class clausal.Literal, so that clients can more readily use it.
-     * 
+     *
      * @return an environment for which the problem evaluates to Bool.TRUE, or
      *         null if no such environment exists.
      */
     public static Environment solve(Formula formula) {
         // TODO: implement this.
-        System.out.println("SAT solver starts!!!");
+
         return solve(formula.getClauses(), new Environment()); //creates new immutable list using clauses from formula
     }
 
     /**
      * Takes a partial assignment of variables to values, and recursively
      * searches for a complete satisfying assignment.
-     * 
+     *
      * @param clauses
      *            formula in conjunctive normal form
      * @param env
@@ -39,10 +39,12 @@ public class SATSolver {
      * @return an environment for which all the clauses evaluate to Bool.TRUE,
      *         or null if no such environment exists.
      */
+    //ORDER IS BOTTOMS UP DUE TO SHAUN'S INTEFERENCE
+    //MINCLAUSE TAKES SMALLEST BOTTOMS UP SO IT WILL GENERALLY END 2-3
     private static Environment solve(ImList<Clause> clauses, Environment env) {
         // TODO: implement this.
         if (clauses.isEmpty()){
-            return env; //if empty clause, clause list is unsolvable. Backtrack. 
+            return env; //if empty clause, clause list is unsolvable. Backtrack.
         }
         int minSize = Integer.MAX_VALUE; //this is a constant. of 2^31-1
         Clause minClause = new Clause();//creates a new clause
@@ -56,34 +58,46 @@ public class SATSolver {
         }
         //now we're going into the clause! Endless Recursion!
         Literal l = minClause.chooseLiteral();//random picking of literal from within list
+        System.out.print("CLAUSE:   ");
+        System.out.print(minClause.toString());
+        System.out.print("     ");
+        System.out.print("Variable: ");
+        System.out.print(l.getVariable().getName());
+        System.out.print("    CHOSE: ");
         Environment result;//this environment is currently undefined
         if (minClause.isUnit()){//if single literal, meaning a ^ (b v c), a is literal chosen
             if (l instanceof PosLiteral){
+                System.out.println("SINGLE PLUS");
                 result = solve(substitute(clauses, l),env.putTrue(l.getVariable()));//positive literal substitute true value and check
+
             } else {
+                System.out.println("SINGLE MINUS");
                 result = solve(substitute(clauses, l),env.putFalse(l.getVariable()));//false of a false is still true. Therefore, subbing in true value
+
             }
         } else{
-            ImList<Clause> temp = substitute(clauses,l);//go into the clause; because more than 2 now. 
+            ImList<Clause> temp = substitute(clauses,l);//go into the clause; because more than 2 now.
             if (l instanceof PosLiteral){
+                System.out.println("MORE PLUS");
                 result = solve(temp, env.putTrue(l.getVariable())); //same thing as above. create a temp clause and check if putting it positive will work
                 if (result != null) return result;
                 result = solve(temp, env.putFalse(l.getVariable()));
             }
             else{
-                result = solve(temp, env.putFalse(l.getVariable()));//not a positive, therefore a negative. reverse of the above. 
+                System.out.println("MORE MINUS");
+                result = solve(temp, env.putFalse(l.getVariable()));//not a positive, therefore a negative. reverse of the above.
                 if (result != null)
                     return result;
                 result = solve(temp,env.putTrue(l.getVariable()));
             }
         }
-        return result;            //the associated variable that we have picked 
-}
+        return result;            //the associated variable that we have picked
+    }
 
     /**
      * given a clause list and literal, produce a new list resulting from
      * setting that literal to true
-     * 
+     *
      * @param clauses
      *            , a list of clauses
      * @param l
@@ -99,7 +113,7 @@ public class SATSolver {
             }
         }
         return result;
-}
+    }
     /*
     ImList<Clause> sub = new EmptyImList<>();
     while(clauses.iterator().hasNext()){//checking to see if there is a notehr member of the clause
